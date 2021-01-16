6e589e22b1a4147665a2af46e084d2b21de1c7b5
Rok <hexgen.games@gmail.com>
2021-01-16 14:00:58 -0600

    Lecture 3: SplashView

--- app/src/main/java/com/udemy/photogallery/mvp/main/MainActivity.kt
+++ app/src/main/java/com/udemy/photogallery/mvp/main/MainActivity.kt
@@ -1,21 +1,12 @@
-package com.udemy.photogallery
+package com.udemy.photogallery.mvp.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import com.udemy.photogallery.R
 
 class MainActivity : AppCompatActivity() {
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
-
     }
-
-
 }
